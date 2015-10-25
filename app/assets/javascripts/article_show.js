4bb777febf9430c321cf28fca624f7777451eded
jamespjbennett <jamespjbennett@hotmail.com>
2015-10-25 21:40:50 +0000

    single image scroll effect working. need to implement second image scroll effect

--- app/assets/javascripts/article_show.js
+++ app/assets/javascripts/article_show.js
@@ -1,8 +1,19 @@
 function checkImagePosition(){
   var scrollTop = $(document).scrollTop();
-  var newWidth = $('.article-image-first-container').width()
-  if(scrollTop >= 595 &&  scrollTop <= 755){
-    $('.article-image-first-container img').css('position', 'fixed').css('top', '20%').width(newWidth);
+  var newWidth = $('.article-image-first-container').width();
+  if(scrollTop >= 595 &&  scrollTop <= 1005){
+    $('.article-image-first-container').show();
+    $('.article-image-first-container img').css('position', 'fixed').css('top', '18%').width(newWidth);
+    $('.article-image-second-container').hide();
+  } else if(scrollTop > 1005 ){
+    var newWidth = $('.article-image-first-container').width();
+    $('.article-image-second-container').show();
+    $('.article-image-first-container').css('margin-top', '39%');
+    $('.article-image-first-container img').css('position', 'absolute').width(newWidth);
+  } else{
+    $('.article-image-first-container img').css('position', "");
+    $('.article-image-first-container').css('margin-top', '0%');
+
   }
 }
 
