87fab35307fba95fb5a3880b1e72bade8c6065bb
jamespjbennett <jamespjbennett@hotmail.com>
2015-10-26 20:02:49 +0000

    adjusted the position at which the fixed image starts scrolling and its catching it much better. Still need to fix the second image

--- app/assets/javascripts/article_show.js
+++ app/assets/javascripts/article_show.js
@@ -1,19 +1,19 @@
 function checkImagePosition(){
   var scrollTop = $(document).scrollTop();
+  var totalHeight = $('html').height() - 595;
   var newWidth = $('.article-image-first-container').width();
-  if(scrollTop >= 595 &&  scrollTop <= 1005){
+  if(scrollTop >= 595 &&  scrollTop <= totalHeight / 2){
     $('.article-image-first-container').show();
     $('.article-image-first-container img').css('position', 'fixed').css('top', '18%').width(newWidth);
     $('.article-image-second-container').hide();
-  } else if(scrollTop > 1005 ){
+  } else if(scrollTop > totalHeight / 2 ){
     var newWidth = $('.article-image-first-container').width();
     var firstImageMargin = parseInt($('.article-image-first-container').css('marginTop'));
     var firstImageHeight = $('.article-image-first-container img').height();
-    var newMargin = firstImageMargin + firstImageHeight;
     $('.article-image-second-container').show();
-    $('.article-image-first-container').css('margin-top', '39%');
-    $('.article-image-second-container').css('margin-top', firstImageHeight + 50);
-    $('.article-image-first-container img').css('position', 'absolute').width(newWidth);
+    $('.article-image-first-container').css('margin-top', totalHeight / 3);
+    $('.article-image-second-container').css('margin-top', '100px');
+    $('.article-image-first-container img').css('position', '').width(newWidth);
   } else{
     $('.article-image-first-container img').css('position', "");
     $('.article-image-first-container').css('margin-top', '0%');
