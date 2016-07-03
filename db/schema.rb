b68fc2b5c14eb7a35ac5ca17dc7647fde8f596c9
James Bennett <jamespjbennett@hotmail.com>
2016-07-03 20:07:46 +0100

    added social icons

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150614204543) do
+ActiveRecord::Schema.define(version: 20160703185608) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20150614204543) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.string   "artist"
+    t.string   "blurb"
   end
 
   create_table "interviews", force: :cascade do |t|
