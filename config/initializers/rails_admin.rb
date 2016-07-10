e1bf366ca6e47c9cdb2fa190ce683345477fcdbf
James Bennett <jamespjbennett@hotmail.com>
2016-07-10 21:57:29 +0100

    text editor for rails admin

---
+++ config/initializers/rails_admin.rb
@@ -0,0 +1,24 @@
+RailsAdmin.config do |config|
+  config.model Article do
+    edit do
+      # For RailsAdmin >= 0.5.0
+      field :title
+      field :content, :wysihtml5
+      field :category
+      field :author
+      field :artist
+      field :blurb
+      field :facebook_uul
+      field :insta_url
+      field :soundcloud_url
+      field :twitter_url
+      field :main_image
+      field :square_image
+      field :rectangular_image
+      # For RailsAdmin < 0.5.0
+      # field :description do
+      #   bootstrap_wysihtml5 true
+      # end
+    end
+  end
+end
