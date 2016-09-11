34093ee7974d882a1ff165563b042169234c5a7f
James Bennett <jamespjbennett@hotmail.com>
2016-09-11 16:39:27 +0100

    even div heights on article show page

--- app/assets/javascripts/article_show.js
+++ app/assets/javascripts/article_show.js
@@ -5,6 +5,12 @@ function checkImagePosition(){
 
 
 $(document).ready(function() {
-  debugger
-  // $(window).on('scroll', checkImagePosition)
+	if(window.location.pathname.indexOf("/articles") != -1){
+
+	$.each($('.artice-small-image-container'), function(index, value){
+		var height = $(value).parent().find('.article-text-container').height();
+		$(value).height(height)
+	})
+  	
+  }
 }); 
