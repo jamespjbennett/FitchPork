38df624c6bcb0dfab569b750cf9f0106270b35db
James Bennett <jamespjbennett@hotmail.com>
2016-09-11 19:09:56 +0100

    few stylistic changes

--- app/assets/javascripts/article_show.js
+++ app/assets/javascripts/article_show.js
@@ -7,10 +7,20 @@ function checkImagePosition(){
 $(document).ready(function() {
 	if(window.location.pathname.indexOf("/articles") != -1){
 
-	$.each($('.artice-small-image-container'), function(index, value){
-		var height = $(value).parent().find('.article-text-container').height();
-		$(value).height(height);
-	})
+		$.each($('.artice-small-image-container'), function(index, value){
+			var height = $(value).parent().find('.article-text-container').height();
+			$(value).height(height);
+		})
+
+		var take_away_margin = ($('body').width() - $('.container').width()) / 2
+		$('.artice-small-image-container-full').width($('body').width());
+		$('.artice-small-image-container-full').css('margin-left', -Math.abs(take_away_margin));
+
+		$(window).resize(function() {
+			var take_away_margin = ($('body').width() - $('.container').width()) / 2
+			$('.artice-small-image-container-full').width($('body').width());
+			$('.artice-small-image-container-full').css('margin-left', -Math.abs(take_away_margin));	
+		})
   	
-  }
+  	}
 }); 
