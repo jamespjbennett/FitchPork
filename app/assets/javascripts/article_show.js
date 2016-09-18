ea4a7c619b362666fbe0c4d380ddf5e3c6b0e1bc
James Bennett <jamespjbennett@hotmail.com>
2016-09-18 19:49:05 +0100

    small css adjustment

--- app/assets/javascripts/article_show.js
+++ app/assets/javascripts/article_show.js
@@ -6,26 +6,27 @@ function checkImagePosition(){
 
 $(document).ready(function() {
 	if(window.location.pathname.indexOf("/articles") != -1){
+		if(!$('.mobile-test').is(':visible')){
+			$.each($('.artice-small-image-container'), function(index, value){
+				var height = $(value).parent().find('.article-text-container').height();
+				$(value).height(height);
+			})
 
-		$.each($('.artice-small-image-container'), function(index, value){
-			var height = $(value).parent().find('.article-text-container').height();
-			$(value).height(height);
-		})
-
-		var take_away_margin = ($('body').width() - $('.container').width()) / 2
-		$('.artice-small-image-container-full').width($('body').width());
-		$('.artice-small-image-container-full').css('margin-left', -Math.abs(take_away_margin));
-		$('.article-header-container ').width($('body').width());
-		$('.article-header-container ').css('margin-left', -Math.abs(take_away_margin));
-		
-
-		$(window).resize(function() {
 			var take_away_margin = ($('body').width() - $('.container').width()) / 2
 			$('.artice-small-image-container-full').width($('body').width());
-			$('.article-header-container ').width($('body').width());
 			$('.artice-small-image-container-full').css('margin-left', -Math.abs(take_away_margin));
+			$('.article-header-container ').width($('body').width());
 			$('.article-header-container ').css('margin-left', -Math.abs(take_away_margin));
-		})
-  	
+			
+
+			$(window).resize(function() {
+				var take_away_margin = ($('body').width() - $('.container').width()) / 2
+				$('.artice-small-image-container-full').width($('body').width());
+				$('.article-header-container ').width($('body').width());
+				$('.artice-small-image-container-full').css('margin-left', -Math.abs(take_away_margin));
+				$('.article-header-container ').css('margin-left', -Math.abs(take_away_margin));
+			})	
+		}
+
   	}
 }); 
